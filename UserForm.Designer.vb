f0ccb76120a2604e8a756f432bd830462dd1a641
chronoo <falax082004@gmail.com>
2024-12-22 18:38:23 +0800

    userform new

--- UserForm.Designer.vb
+++ UserForm.Designer.vb
@@ -22,16 +22,21 @@ Partial Class UserForm
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(UserForm))
         btn_withdraw = New Button()
         btn_deposit = New Button()
-        btn_Loan = New Button()
         Label1 = New Label()
         btn_history = New Button()
         btn_transfer = New Button()
-        btn_bills = New Button()
         Lbl_AvailBal = New Label()
         btn_logout = New Button()
         Label2 = New Label()
+        PictureBox2 = New PictureBox()
+        PictureBox3 = New PictureBox()
+        PictureBox4 = New PictureBox()
+        CType(PictureBox2, ComponentModel.ISupportInitialize).BeginInit()
+        CType(PictureBox3, ComponentModel.ISupportInitialize).BeginInit()
+        CType(PictureBox4, ComponentModel.ISupportInitialize).BeginInit()
         SuspendLayout()
         ' 
         ' btn_withdraw
@@ -40,10 +45,10 @@ Partial Class UserForm
         btn_withdraw.BackgroundImageLayout = ImageLayout.None
         btn_withdraw.Font = New Font("Arial Rounded MT Bold", 15.75F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
         btn_withdraw.ForeColor = SystemColors.ButtonFace
-        btn_withdraw.Location = New Point(25, 359)
+        btn_withdraw.Location = New Point(186, 528)
         btn_withdraw.Margin = New Padding(3, 4, 3, 4)
         btn_withdraw.Name = "btn_withdraw"
-        btn_withdraw.Size = New Size(155, 110)
+        btn_withdraw.Size = New Size(155, 60)
         btn_withdraw.TabIndex = 0
         btn_withdraw.Text = "Withdraw"
         btn_withdraw.UseVisualStyleBackColor = False
@@ -54,26 +59,13 @@ Partial Class UserForm
         btn_deposit.Font = New Font("Arial Rounded MT Bold", 15.75F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
         btn_deposit.ForeColor = SystemColors.ButtonFace
         btn_deposit.ImageAlign = ContentAlignment.BottomLeft
-        btn_deposit.Location = New Point(186, 359)
+        btn_deposit.Location = New Point(21, 528)
         btn_deposit.Margin = New Padding(3, 4, 3, 4)
         btn_deposit.Name = "btn_deposit"
-        btn_deposit.Size = New Size(155, 110)
+        btn_deposit.Size = New Size(155, 60)
         btn_deposit.TabIndex = 1
         btn_deposit.Text = "Deposit"
         btn_deposit.UseVisualStyleBackColor = False
-        ' 
-        ' btn_Loan
-        ' 
-        btn_Loan.BackColor = SystemColors.ActiveCaptionText
-        btn_Loan.Font = New Font("Arial Rounded MT Bold", 15.75F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
-        btn_Loan.ForeColor = SystemColors.ButtonFace
-        btn_Loan.Location = New Point(25, 571)
-        btn_Loan.Margin = New Padding(3, 4, 3, 4)
-        btn_Loan.Name = "btn_Loan"
-        btn_Loan.Size = New Size(155, 110)
-        btn_Loan.TabIndex = 2
-        btn_Loan.Text = "Loan"
-        btn_Loan.UseVisualStyleBackColor = False
         ' 
         ' Label1
         ' 
@@ -91,12 +83,12 @@ Partial Class UserForm
         ' btn_history
         ' 
         btn_history.BackColor = SystemColors.ActiveCaptionText
-        btn_history.Font = New Font("Arial Rounded MT Bold", 15.75F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        btn_history.Font = New Font("Arial Rounded MT Bold", 10.2F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
         btn_history.ForeColor = SystemColors.ButtonFace
-        btn_history.Location = New Point(347, 571)
+        btn_history.Location = New Point(398, 100)
         btn_history.Margin = New Padding(3, 4, 3, 4)
         btn_history.Name = "btn_history"
-        btn_history.Size = New Size(155, 110)
+        btn_history.Size = New Size(117, 30)
         btn_history.TabIndex = 7
         btn_history.Text = "History"
         btn_history.UseVisualStyleBackColor = False
@@ -106,26 +98,13 @@ Partial Class UserForm
         btn_transfer.BackColor = SystemColors.ActiveCaptionText
         btn_transfer.Font = New Font("Arial Rounded MT Bold", 15.75F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
         btn_transfer.ForeColor = SystemColors.ButtonFace
-        btn_transfer.Location = New Point(347, 359)
+        btn_transfer.Location = New Point(353, 528)
         btn_transfer.Margin = New Padding(3, 4, 3, 4)
         btn_transfer.Name = "btn_transfer"
-        btn_transfer.Size = New Size(155, 110)
+        btn_transfer.Size = New Size(155, 60)
         btn_transfer.TabIndex = 9
         btn_transfer.Text = "Transfer"
         btn_transfer.UseVisualStyleBackColor = False
-        ' 
-        ' btn_bills
-        ' 
-        btn_bills.BackColor = SystemColors.ActiveCaptionText
-        btn_bills.Font = New Font("Arial Rounded MT Bold", 15.75F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
-        btn_bills.ForeColor = SystemColors.ButtonFace
-        btn_bills.Location = New Point(186, 571)
-        btn_bills.Margin = New Padding(3, 4, 3, 4)
-        btn_bills.Name = "btn_bills"
-        btn_bills.Size = New Size(155, 110)
-        btn_bills.TabIndex = 10
-        btn_bills.Text = "Bills"
-        btn_bills.UseVisualStyleBackColor = False
         ' 
         ' Lbl_AvailBal
         ' 
@@ -143,8 +122,9 @@ Partial Class UserForm
         ' btn_logout
         ' 
         btn_logout.BackColor = SystemColors.ActiveCaptionText
+        btn_logout.Font = New Font("Arial Rounded MT Bold", 9F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
         btn_logout.ForeColor = SystemColors.ButtonFace
-        btn_logout.Location = New Point(380, 760)
+        btn_logout.Location = New Point(405, 742)
         btn_logout.Margin = New Padding(3, 4, 3, 4)
         btn_logout.Name = "btn_logout"
         btn_logout.Size = New Size(110, 53)
@@ -164,40 +144,78 @@ Partial Class UserForm
         Label2.TabIndex = 13
         Label2.Text = "₱"
         ' 
+        ' PictureBox2
+        ' 
+        PictureBox2.BackColor = SystemColors.Control
+        PictureBox2.Image = CType(resources.GetObject("PictureBox2.Image"), Image)
+        PictureBox2.Location = New Point(21, 365)
+        PictureBox2.Name = "PictureBox2"
+        PictureBox2.Size = New Size(155, 156)
+        PictureBox2.SizeMode = PictureBoxSizeMode.StretchImage
+        PictureBox2.TabIndex = 15
+        PictureBox2.TabStop = False
+        ' 
+        ' PictureBox3
+        ' 
+        PictureBox3.Image = CType(resources.GetObject("PictureBox3.Image"), Image)
+        PictureBox3.Location = New Point(186, 365)
+        PictureBox3.Name = "PictureBox3"
+        PictureBox3.Size = New Size(155, 156)
+        PictureBox3.SizeMode = PictureBoxSizeMode.StretchImage
+        PictureBox3.TabIndex = 16
+        PictureBox3.TabStop = False
+        ' 
+        ' PictureBox4
+        ' 
+        PictureBox4.Image = CType(resources.GetObject("PictureBox4.Image"), Image)
+        PictureBox4.Location = New Point(353, 365)
+        PictureBox4.Name = "PictureBox4"
+        PictureBox4.Size = New Size(155, 156)
+        PictureBox4.SizeMode = PictureBoxSizeMode.StretchImage
+        PictureBox4.TabIndex = 17
+        PictureBox4.TabStop = False
+        ' 
         ' UserForm
         ' 
         AutoScaleDimensions = New SizeF(8F, 20F)
         AutoScaleMode = AutoScaleMode.Font
         BackgroundImage = My.Resources.Resources.Black_Aesthetic_Motivational_Quote_Your_Story__4_
-        ClientSize = New Size(527, 863)
+        ClientSize = New Size(527, 808)
         Controls.Add(btn_logout)
         Controls.Add(Lbl_AvailBal)
-        Controls.Add(btn_bills)
         Controls.Add(btn_transfer)
         Controls.Add(btn_history)
         Controls.Add(Label1)
-        Controls.Add(btn_Loan)
         Controls.Add(btn_deposit)
         Controls.Add(btn_withdraw)
         Controls.Add(Label2)
+        Controls.Add(PictureBox2)
+        Controls.Add(PictureBox3)
+        Controls.Add(PictureBox4)
         Margin = New Padding(3, 4, 3, 4)
         Name = "UserForm"
+        ShowIcon = False
+        ShowInTaskbar = False
         Text = "Banking APP"
+        CType(PictureBox2, ComponentModel.ISupportInitialize).EndInit()
+        CType(PictureBox3, ComponentModel.ISupportInitialize).EndInit()
+        CType(PictureBox4, ComponentModel.ISupportInitialize).EndInit()
         ResumeLayout(False)
         PerformLayout()
     End Sub
 
     Friend WithEvents btn_withdraw As Button
     Friend WithEvents btn_deposit As Button
-    Friend WithEvents btn_Loan As Button
     Friend WithEvents Label1 As Label
     Friend WithEvents btn_rewards As Button
     Friend WithEvents btn_credit As Button
     Friend WithEvents btn_history As Button
     Friend WithEvents btn_invest As Button
     Friend WithEvents btn_transfer As Button
-    Friend WithEvents btn_bills As Button
     Friend WithEvents Lbl_AvailBal As Label
     Friend WithEvents btn_logout As Button
     Friend WithEvents Label2 As Label
+    Friend WithEvents PictureBox2 As PictureBox
+    Friend WithEvents PictureBox3 As PictureBox
+    Friend WithEvents PictureBox4 As PictureBox
 End Class
